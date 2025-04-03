82d117ccad846059893a6725be98672b9dbc995e
28-oi <79718245+28oi@users.noreply.github.com>
2025-04-03 22:02:23 +0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <button className="btn btn-primary animated-btn">Пример кнопки с анимацией</button>
+
+
+
+    </div>
+  );
+}
+
+export default App;
